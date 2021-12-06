df37d3c4d83c9bcd1f899afd7286958c571a9072
NegaTiv1298 <levkristi@i.ua>
2021-12-06 21:51:35 +0200

    Lesson(1)Ex(3)

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
+<html>
+<head>
+    <title>Задача 3</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+
 <?php
-echo 123;
+
+$time = date('H', time());
+
+if ($time >= 3 and $time < 11) {
+    echo "Добрий ранок, світ!";
+} else {
+    echo "Привіт, світ!";
+}
+?>
+
+</body>
+</html>
