9e5dc5e095e08ed605434c1b058a46e548356e28
NegaTiv1298 <levkristi@i.ua>
2021-12-13 22:55:16 +0200

    Lesson(2)Part(1)Ex(2)

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
 <html>
 <head>
-    <title>Задача 3</title>
+    <title>Форма</title>
     <meta charset="UTF-8">
 </head>
 <body>
 
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+    Email: <input type="text" name="email"><br>
+    <input type="submit">
+</form>
 <?php
+if (isset($_POST['email'])) {
 
-$time = date('H', time());
-
-if ($time >= 3 and $time < 11) {
-    echo "Добрий ранок, світ!";
-} else {
-    echo "Привіт, світ!";
+    $email = trim($_POST['email']);
+    $searchSymbol = strpos($email, '@');
+    $searchDot = strpos($email, '.', $searchSymbol);
+    $searchEndDot = strpos($email, '.', -1);
+    if ($searchSymbol === false or $searchDot === false or $searchEndDot !== false) {
+        echo 'Email не коректний';
+    } else {
+        echo 'Email коректний';
+    }
 }
 ?>
 
