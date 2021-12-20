6008e19d84921908566d0331aa1d17f9cc1c010c
NegaTiv1298 <levkristi@i.ua>
2021-12-20 23:06:59 +0200

    Lesson(3)Ex(1)

--- index.php
+++ index.php
@@ -6,20 +6,48 @@
 <body>
 
 <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-    Текст: <input type="text" name="text"><br>
+    <select name="fuel">
+        <option value="diesel">Дизель</option>
+        <option value="petrol">Бензин</option>
+    </select><br>
+    Об'єм двигуна: <input type="text" name="volume" placeholder="см.куб"><br>
+    Рік випуску автомобіля: <input type="number" min="1970" max="2022" name="graduation"><br>
+    Вартість автомобіля: <input type="text" name="price"><br>
     <input type="submit">
 </form>
 <?php
-if (!empty($_POST['text'])) {
-
-    $text = $_POST['text'];
-    $resultTest = preg_replace('"\b(https?://\S+)"', "<a href='$1'>$1</a>", $text);
-
-    echo $resultTest;
+if (empty($_POST['fuel']) || empty($_POST['volume']) || empty($_POST['graduation']) || empty($_POST['price'])) {
+    echo 'Всі поля обов\'язкові';
 } else {
-    echo 'Заповніть форму';
+    $volume = $_POST['volume'];
+    $graduation = $_POST['graduation'];
+    $price = $_POST['price'];
+
+    if ($volume < '500') {
+        echo 'Не правильний об\'єм двигуна';
+    } else {
+        if ($_POST['fuel'] == 'diesel') {
+            $baseRate = 75;
+        } else {
+            $baseRate = 50;
+        }
+        $currDate = date('Y', time());
+        $diff = $currDate - $graduation - 1;
+
+        $engineRatio = round($volume / 1000);
+
+        if ($diff == 1) {
+            $ageRatio = 1;
+        } else {
+            $ageRatio = $diff;
+        }
+        $customsClearance = $baseRate * $engineRatio * $ageRatio;
+        $resultPriceCar = $price + $customsClearance;
+
+        echo "Розмитнення автомобіля = $customsClearance євро" . "<br>";
+        echo "Загальна вартість автомобіля = $resultPriceCar євро";
+    }
 }
 ?>
-
 </body>
 </html>
