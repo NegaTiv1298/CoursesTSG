654d6f790dfc79b9317716e94d26ca36c6fffdd0
NegaTiv1298 <levkristi@i.ua>
2021-12-21 17:11:13 +0200

    Lesson(3)Ex(2)

--- index.php
+++ index.php
@@ -1,31 +1,6 @@
-<html>
-<head>
-    <title>Форма</title>
-    <meta charset="UTF-8">
-</head>
-<body>
-
-<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-    Кількість елементів піраміди: <input type="text" name="qty_elem">
-    <input type="submit">
-</form>
 <?php
-if (empty($_POST['qty_elem'])) {
-    echo 'Заповніть поле форми';
-} else {
-    $qtyElem = $_POST['qty_elem'];
-    if ($qtyElem > 15) {
-        echo 'Неправильна висота піраміди ( від 1 до 15 )';
-    } else {
-        $elem = '##';
-        for ($i = 1; $i <= $qtyElem; $i++) {
-            for ($j = 1; $j <= $i; $j++) {
-                echo $elem;
-            }
-            echo '<br>';
-        }
+for($i = 2000; $i <= 2100; $i++) {
+    if($i % 4 == 0) {
+        echo $i.'<br>';
     }
 }
-?>
-</body>
-</html>
