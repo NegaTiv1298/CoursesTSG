8bd43dacc5ac414a1b0f1f3982389b84097777f3
NegaTiv1298 <levkristi@i.ua>
2021-12-27 09:29:59 +0200

    Lesson(2)Part(2)Ex(2)

--- lesson2p2ex2.php
+++ lesson2p2ex2.php
@@ -1,1 +1,14 @@
+<body>
+
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+    Текст: <input type="text" name="text"><br>
+    <input type="submit">
+</form>
 <?php
+if (!empty($_POST['text'])) {
+    $text = $_POST['text'];
+    $resultTest = preg_replace('"\b(https?://\S+)"', "<a href='$1'>$1</a>", $text);
+    echo $resultTest;
+} else {
+    echo 'Заповніть форму';
+}
